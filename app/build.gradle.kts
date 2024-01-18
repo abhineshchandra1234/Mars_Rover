0c62cc43e35b5d945b2aac35218af25c1c925b18
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-18 09:06:25 +0530

    created data models and added retrofit instance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -52,7 +54,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -70,4 +72,17 @@ dependencies {
 
     //navigation compose
     implementation("androidx.navigation:navigation-compose:2.7.6")
+
+    //retrofit
+    val retrofit = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofit")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //Hilt
+    val hilt_version = "2.50"
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
---
+++ app/src/main/java/com/example/marsrover/service/MarsRoverManifestService.kt
@@ -0,0 +1,35 @@
+package com.example.marsrover.service
+
+import com.example.marsrover.service.model.RoverManifestRemoteModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MarsRoverManifestService {
+
+    @GET("mars-photos/api/v1/manifest/{rover_name}?api_key=UJuCZAoCw2hFh8aaRRPe9zgimzHXqY85gZuN2xjJ")
+    suspend fun getMarsRoverManifest(@Path("rover_name") roverName: String): RoverManifestRemoteModel
+
+    companion object {
+        private const val BASE_URL = "https://api.nasa.gov/"
+
+        fun create(): MarsRoverManifestService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(MarsRoverManifestService::class.java)
+        }
+    }
+}

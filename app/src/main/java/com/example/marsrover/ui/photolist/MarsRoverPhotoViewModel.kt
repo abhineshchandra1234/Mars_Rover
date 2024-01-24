5cb3f5b496981464fa45301bc09fea62d292cf0c
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-24 09:36:26 +0530

    displaying data for each sol for each rover

--- app/src/main/java/com/example/marsrover/ui/photolist/MarsRoverPhotoViewModel.kt
+++ app/src/main/java/com/example/marsrover/ui/photolist/MarsRoverPhotoViewModel.kt
@@ -3,8 +3,10 @@ package com.example.marsrover.ui.photolist
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.marsrover.data.MarsRoverPhotoRepo
+import com.example.marsrover.domain.model.RoverPhotoUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,10 +15,17 @@ class MarsRoverPhotoViewModel @Inject constructor(
     private val marsRoverPhotoRepo: MarsRoverPhotoRepo
 ) : ViewModel() {
 
+    private val _roverPhotoUiState: MutableStateFlow<RoverPhotoUiState> =
+        MutableStateFlow(RoverPhotoUiState.Loading)
+
+    val roverPhotoUiState: StateFlow<RoverPhotoUiState>
+        get() = _roverPhotoUiState
+
     fun getMarsRoverPhoto(roverName: String, sol: String) {
         viewModelScope.launch {
+            _roverPhotoUiState.value = RoverPhotoUiState.Loading
             marsRoverPhotoRepo.getMarsRoverPhoto(roverName, sol).collect {
-                
+                _roverPhotoUiState.value = it
             }
         }
     }
--- app/src/main/java/com/example/marsrover/ui/view/PhotoList.kt
+++ app/src/main/java/com/example/marsrover/ui/view/PhotoList.kt
@@ -1,18 +1,37 @@
 package com.example.marsrover.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.marsrover.R
 import com.example.marsrover.domain.model.RoverPhotoUiModel
 
 
 @Composable
-fun PhotoList() {
+fun PhotoList(
+    roverPhotoUiModelList: List<RoverPhotoUiModel>
+) {
+
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn {
+            items(count = roverPhotoUiModelList.size, itemContent = { index ->
+                Photo(roverPhotoUiModel = roverPhotoUiModelList[index])
+            })
+        }
+    }
 }
 
 @Composable
@@ -26,6 +45,9 @@ fun Photo(
             modifier = Modifier.padding(16.dp)
         ) {
             Text(text = roverPhotoUiModel.roverName, modifier = Modifier.padding(16.dp))
+            Text(text = stringResource(id = R.string.sol, roverPhotoUiModel.sol))
+            Text(text = stringResource(id = R.string.earth_date, roverPhotoUiModel.earthDate))
+            Text(text = roverPhotoUiModel.cameraFullName)
         }
     }
 }

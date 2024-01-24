31d9fe0daffee5d62d64de5865df2f153e60035d
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-24 16:30:39 +0530

    fetched images for each sol for each rover

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,5 +90,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     //coil
-    implementation("io.coil-kt:coil:2.5.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
--- app/src/main/java/com/example/marsrover/ui/view/PhotoList.kt
+++ app/src/main/java/com/example/marsrover/ui/view/PhotoList.kt
@@ -2,6 +2,7 @@ package com.example.marsrover.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.marsrover.R
 import com.example.marsrover.domain.model.RoverPhotoUiModel
 
@@ -45,6 +47,13 @@ fun Photo(
             modifier = Modifier.padding(16.dp)
         ) {
             Text(text = roverPhotoUiModel.roverName, modifier = Modifier.padding(16.dp))
+
+            AsyncImage(
+                model = roverPhotoUiModel.imgSrc,
+                contentDescription = "rover photo",
+                modifier = Modifier.height(300.dp)
+                )
+
             Text(text = stringResource(id = R.string.sol, roverPhotoUiModel.sol))
             Text(text = stringResource(id = R.string.earth_date, roverPhotoUiModel.earthDate))
             Text(text = roverPhotoUiModel.cameraFullName)
